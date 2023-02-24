261669b2a4aff9019312fc51d4c8b3517774bae2
xkuadrattt <calmystu.00@gmail.com>
2023-02-24 15:05:50 +0700

    fixed white space

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ImgRight from "./components/ImgRight";
 
 function App() {
   return (
-    <main className="md:grid md:grid-cols-layout">
+    <main className="md:grid md:grid-cols-layout min-h-screen">
       <HeadlineLeft />
       <ImgRight />
     </main>
--- src/components/section/Formsubs.tsx
+++ src/components/section/Formsubs.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function Formsubs() {
   return (
-    <div className="mail flex flex-col gap-[1.25rem] mb-[9.75rem]">
+    <div className="mail flex flex-col gap-[1.25rem] pb-[9.75rem] w-full">
       <form action="POST" className="flex flex-wrap gap-3">
         <input
           type="text"
           placeholder="Email Address"
           name="email"
-          className="p-2 rounded-lg ring-1 ring-blue-200 max-w-[20.8rem]"
+          className="rounded-lg ring-1 placeholder-primary/60 py-1 px-2 sm:py-[.625rem] sm:px-[1.75rem] w-auto"
         />
-        <button className="py-[.5rem] px-[1rem] bg-accent hover:bg-accent/90 text-white rounded-lg">
+        <button className="py-1 px-3 sm:py-[.625rem] sm:px-[1.15rem] md:px-[1.75rem] bg-accent hover:bg-accent/90 text-white rounded-lg">
           Invite Me
         </button>
       </form>
-      <p className="leading-8 text-primary/60">
+      <p className="leading-8 text-primary/60 max-w-[33.8rem]">
         Sign up for updates to be the first to know when we launch. No spam,
         just important information and exclusive offers.
       </p>
